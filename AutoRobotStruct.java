cc0106837e25b3e4246bbbe1f0e370f9db9a7e1d
QuinnRainey314 <90530493+QuinnRainey314@users.noreply.github.com>
2024-11-08 08:55:31 -0500

    Update AutoRobotStruct.java

--- AutoRobotStruct.java
+++ AutoRobotStruct.java
@@ -22,52 +22,21 @@ public class AutoRobotStruct extends LinearOpMode {
     public DcMotor motorFrontLeft;
     public DcMotor motorBackRight;
     public DcMotor motorBackLeft;
-    public DcMotor Arm1;
-    public DcMotor Arm2;
-    public DcMotor chainMotor;
-    public Servo servoClaw1;
-    public Servo servoClaw2;
-    public Servo launcher;
-    public Servo tapeHold;
-    public Servo tapeRelease;
     public ColorSensor colorSensor; // CH I2C 2
-    public boolean bLedOn = true;
-    public boolean atLine = false;
-    public String estColor;
-    public int position = 0;
-    public TouchSensor leftSensor;
-    public TouchSensor rightSensor;
-
+    
     @Override
     public void runOpMode() throws InterruptedException {}
 
     public void initRunner() throws InternalError {
-         gyroSensor = hardwareMap.get(GyroSensor.class, "gyroSensor"); // EH I2C 2
-        servoClaw1 = hardwareMap.get(Servo.class, "servoClaw1"); // EH 5
-        servoClaw2 = hardwareMap.get(Servo.class, "servoClaw2"); // EH 2
-        tapeRelease = hardwareMap.get(Servo.class, "tapeRelease"); // EH 3
-        tapeHold = hardwareMap.get(Servo.class, "tapeHold"); // EH 4
-        dragBlock = hardwareMap.get(Servo.class, "dragBlock"); // EH 1 
-        launcher = hardwareMap.get(Servo.class, "launcher"); // EH 0
+
         motorFrontLeft = hardwareMap.get(DcMotor.class, "motorFrontLeft"); // 3
         motorFrontRight = hardwareMap.get(DcMotor.class, "motorFrontRight"); // 2
         motorBackLeft = hardwareMap.get(DcMotor.class, "motorBackLeft"); // 1
         motorBackRight = hardwareMap.get(DcMotor.class, "motorBackRight"); // 0
         WebcamName webcam1 = hardwareMap.get(WebcamName.class, "Webcam1"); // Adjust name if needed
         
-        Arm1 = hardwareMap.get(DcMotor.class, "Arm1"); 
-        Arm2 = hardwareMap.get(DcMotor.class, "Arm2"); 
-        chainMotor = hardwareMap.get(DcMotor.class, "chainMotor"); 
-        Arm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        Arm2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        chainMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         
-        dragBlock.setPosition(0.05);
-        // launcher.setPosition(.9);
-        // servoClaw2.setPosition(.705);   
-        // servoClaw1.setPosition(.375);
-        // tapeRelease.setPosition(0.43);
-        // tapeHold.setPosition(0.7);
+
         motorBackRight.setDirection(DcMotor.Direction.REVERSE);
         motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
     }
